7fade08b474922421ed7b22824aead509fdd1a19
Velcar1 <abertopineda@gmail.com>
2024-08-28 16:50:18 -0400

    borrar prueba

--- gitllave.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Iniciar el agente SSH y exportar la variable de entorno necesaria
-eval $(ssh-agent -s)
-
-# Agregar la clave privada especificada al agente SSH
-ssh-add ~/.ssh/id_rsa_ed25519
