57dea6aa928bbad0919bc2ba9d84781ea3a591b0
Velcar1 <abertopineda@gmail.com>
2024-09-07 18:09:15 -0400

    Notificacion de bateria baja

--- bateria.sh
+++ bateria.sh
@@ -48,3 +48,7 @@ fi
 # Mostrar el resultado entre 0 y 100, con símbolo de carga si aplica
 echo "   $avg_percentage%$charging_symbol"
 
+# Enviar notificación si el promedio de batería es menor al 10%
+if [ "$avg_percentage" -lt 10 ]; then
+    notify-send "Advertencia de batería baja" "La batería está por debajo del 10% ($avg_percentage%)"
+fi
